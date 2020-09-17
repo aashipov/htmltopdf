99ed1082d46a486fac62ae1e875efb5d5aca6f49
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2020-09-17 07:18:29 +0300

    msg

--- test/farm/farm-refresh.bash
+++ test/farm/farm-refresh.bash
@@ -16,7 +16,7 @@ VOLUMES_HAPROXY="-v ${THIS_DIR}/haproxy/:/usr/local/etc/haproxy/:ro"
 
 docker pull ${HTML_TO_PDF_IMAGE}
 docker pull ${HAPROXY_IMAGE}
-source ${THIS_DIR}/down.bash
+source ./down.bash
 
 docker network create -d bridge ${NETWORK_NAME}
 docker run -d --name=${NODE_1} --hostname=${NODE_1} --net=${NETWORK_NAME} ${HTML_TO_PDF_IMAGE}
