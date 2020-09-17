f4dfd8b15be618564346bd4710fa4a7e2123fe49
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2020-09-17 07:40:23 +0300

    msg

--- test/farm/farm-refresh.bash
+++ test/farm/farm-refresh.bash
@@ -5,18 +5,17 @@ set -x
 
 HTML_TO_PDF_IMAGE="aashipov/htmltopdf"
 HAPROXY_IMAGE="haproxy:1.7"
-# /$(pwd) to expand properly on MS Windows
-THIS_DIR=/$(pwd)
+THIS_DIR=$(pwd)
 NODE_1=htmltopdf1
 NODE_2=htmltopdf2
 NODE_3=htmltopdf3
 HAPROXY=haproxy
 NETWORK_NAME=htmltopdf
-VOLUMES_HAPROXY="-v ${THIS_DIR}/haproxy/:/usr/local/etc/haproxy/:ro"
+VOLUMES_HAPROXY="-v /${THIS_DIR}/haproxy/:/usr/local/etc/haproxy/:ro"
 
 docker pull ${HTML_TO_PDF_IMAGE}
 docker pull ${HAPROXY_IMAGE}
-source ./down.bash
+source ${THIS_DIR}/down.bash
 
 docker network create -d bridge ${NETWORK_NAME}
 docker run -d --name=${NODE_1} --hostname=${NODE_1} --net=${NETWORK_NAME} ${HTML_TO_PDF_IMAGE}
