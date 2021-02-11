ad67b48179872f0f49781ecb36650071ed8aa380
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-02-11 14:36:49 +0300

    msg
    (cherry picked from commit afd810f8db5840d0da01cd5c222b9e0e063d02d2)

--- build-and-run.bash
+++ build-and-run.bash
@@ -3,8 +3,8 @@
 set -x
 
 #To ease copy-paste
-CONTAINER_NAME=htmltopdf
+CONTAINER_NAME=htmltopdf-dev
 
-docker build . --file=Dockerfile --tag=${CONTAINER_NAME}
+docker build --file=Dockerfile --tag=${CONTAINER_NAME} .
 source ./down.bash
 docker run -d --name=${CONTAINER_NAME} --hostname=${CONTAINER_NAME} -p 8080:8080 ${CONTAINER_NAME}
