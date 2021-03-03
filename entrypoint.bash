013b470ee2bdc6f21f817871c26327bef9963129
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-03-03 10:12:30 +0300

    --no-zygote

--- entrypoint.bash
+++ entrypoint.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Burden the container with chromium (re)start
-chromium --remote-debugging-port=9222 --headless --no-sandbox --disable-setuid-sandbox --disable-notifications --disable-geolocation --disable-infobars --disable-session-crashed-bubble --disable-dev-shm-usage --disable-gpu --disable-translate --disable-extensions --disable-background-networking  --disable-sync --disable-default-apps --hide-scrollbars --metrics-recording-only --mute-audio --no-first-run --unlimited-storage --safebrowsing-disable-auto-update --font-render-hinting=none &
+chromium --remote-debugging-port=9222 --headless --no-sandbox --no-zygote --disable-setuid-sandbox --disable-notifications --disable-geolocation --disable-infobars --disable-session-crashed-bubble --disable-dev-shm-usage --disable-gpu --disable-translate --disable-extensions --disable-background-networking  --disable-sync --disable-default-apps --hide-scrollbars --metrics-recording-only --mute-audio --no-first-run --unlimited-storage --safebrowsing-disable-auto-update --font-render-hinting=none &
 
 ./htmltopdf
