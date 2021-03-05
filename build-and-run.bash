fd0b77386e38ac86d76f1e576c6e4305e41aca10
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-03-05 13:12:08 +0300

    msg

--- build-and-run.bash
+++ build-and-run.bash
@@ -11,4 +11,5 @@ docker build --file=Dockerfile --tag=${IMAGE}:latest --tag=${IMAGE}:${TOP_COMMIT
 source ./down.bash
 docker run -d --name=${CONTAINER_NAME} --hostname=${CONTAINER_NAME} -p 8080:8080 ${IMAGE}:${TOP_COMMIT}
 # Push VCS commit sha as docker hub tag to bypass nexus bug
-docker push ${IMAGE} --all-tags
+docker push --tag=${IMAGE}:latest
+docker push --tag=${IMAGE}:${TOP_COMMIT}
