f6cecee642fd0544e662900ad8964f894787fe31
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-03-06 02:49:52 +0300

    base

--- build-and-run-base.bash
+++ build-and-run-base.bash
@@ -5,5 +5,5 @@ set -x
 #To ease copy-paste
 IMAGE="aashipov/htmltopdf:base"
 docker pull ${IMAGE}
-docker build --file=Dockerfile --tag=${IMAGE} .
+docker build --file=Dockerfile-base --tag=${IMAGE} .
 docker push ${IMAGE}
