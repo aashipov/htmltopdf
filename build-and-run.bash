29e424ede54d527000e1f840575307d98856607e
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-03-05 13:24:50 +0300

    msg

--- build-and-run.bash
+++ build-and-run.bash
@@ -3,8 +3,14 @@
 set -x
 
 #To ease copy-paste
+IMAGE="aashipov/htmltopdf"
 CONTAINER_NAME=htmltopdf-dev
+TOP_COMMIT=$(git log --pretty=format:'%h' -n 1)
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-docker build --file=Dockerfile --tag=${CONTAINER_NAME} .
+docker build --file=Dockerfile --tag=${IMAGE}:${TOP_COMMIT} --tag=${IMAGE}:${CURRENT_BRANCH} .
 source ./down.bash
-docker run -d --name=${CONTAINER_NAME} --hostname=${CONTAINER_NAME} -p 8080:8080 ${CONTAINER_NAME}
+docker run -d --name=${CONTAINER_NAME} --hostname=${CONTAINER_NAME} -p 8080:8080 ${IMAGE}:${TOP_COMMIT}
+# Push VCS commit sha as docker hub tag to bypass nexus bug
+docker push ${IMAGE}:${TOP_COMMIT}
+docker push ${IMAGE}:${CURRENT_BRANCH}
