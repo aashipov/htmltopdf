cbbb7a77e9a848c042d7a613722afef3023ab178
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-03-17 17:59:30 +0300

    simplified

--- build-base.bash
+++ build-base.bash
@@ -5,5 +5,5 @@ set -e
 #To ease copy-paste
 IMAGE="aashipov/htmltopdf:base"
 docker pull ${IMAGE}
-docker build --file=Dockerfile-base --tag=${IMAGE} .
+docker build --file=Dockerfile.base --tag=${IMAGE} .
 docker push ${IMAGE}
