916dd34140dbe7d71a48a61b7b58ddc0e29124db
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-05-24 10:23:40 +0300

    msg

--- build-base.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-#To ease copy-paste
-IMAGE="aashipov/htmltopdf:base"
-docker build --file=Dockerfile.base --tag=${IMAGE} .
-docker push ${IMAGE}
--- build-buildbed.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-#To ease copy-paste
-IMAGE="aashipov/htmltopdf:buildbed"
-docker build --file=Dockerfile.buildbed --tag=${IMAGE} .
-docker push ${IMAGE}
