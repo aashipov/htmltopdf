5d70fcd976a1672eac08ea35a51881cb9fb79153
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2021-05-24 10:26:42 +0300

    msg

--- build-and-run.bash
+++ build-and-run.bash
@@ -8,6 +8,7 @@ CONTAINER_NAME=htmltopdf-dev
 TOP_COMMIT=$(git log --pretty=format:'%h' -n 1)
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
+docker pull ${IMAGE}
 docker build --file=Dockerfile --tag=${IMAGE}:latest --tag=${IMAGE}:${TOP_COMMIT} --tag=${IMAGE}:${CURRENT_BRANCH} .
 source ./down.bash
 docker run -d --name=${CONTAINER_NAME} --hostname=${CONTAINER_NAME} -p 8080:8080 ${IMAGE}:${TOP_COMMIT}
